d3cb20693dc6eda120b9b9da535d155d59b9f463
Krystian Pietroń <krystianpietr@gmail.com>
2018-11-30 11:31:14 +0100

    add constructor from Snake.js

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -2,26 +2,53 @@ import React from 'react'
 import GameBoard from './GameBoard'
 
 class Snake extends React.Component {
-    state = {
-        gameBoard: (
-            Array(this.props.boardDimension)
-                .fill(
-                    Array(this.props.boardDimension)
-                        .fill(1)
-                )
-        ),
-        snake1: [{
-            x:5,
-            y:5
-        }]
+    constructor(props) {
+        super(props)
+
+        const halfBoardDimension = Math.ceil(props.boardDimension / 2) - 1
+
+        this.state = {
+            gameBoard: (
+                Array(this.props.boardDimension)
+                    .fill(
+                        Array(this.props.boardDimension)
+                            .fill(1)
+                    )
+            ),
+            snakes: [
+                [
+                    { x: halfBoardDimension + 2, y: halfBoardDimension },
+                    { x: halfBoardDimension + 1, y: halfBoardDimension },
+                ],
+                [
+                    { x: halfBoardDimension - 2, y: halfBoardDimension },
+                    { x: halfBoardDimension - 1, y: halfBoardDimension }
+                ]
+            ],
+            directions: [
+                'right',
+                'left'
+            ],
+            food: [
+                // { x: 2, y: 2 }
+            ],
+            currentPlayerIndex: 0
+        }
+
     }
-composeGameBoard = () => {
-    const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
-    this.state.snake1.forEach(bodyCellPosition=>(
-        gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x]=0
-    ))
-    return gameBoardCopy
-}
+    composeGameBoard = () => {
+        const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
+        this.state.snakes.forEach(snake => (
+            snake.forEach(bodyCellPosition => (
+                gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
+            ))
+        ))
+        this.state.food.forEach(foodPosition => (
+            gameBoardCopy[foodPosition.y][foodPosition.x] = 'F'
+        ))
+        return gameBoardCopy
+    }
+
     render() {
         const gameBoard = this.composeGameBoard()
 
