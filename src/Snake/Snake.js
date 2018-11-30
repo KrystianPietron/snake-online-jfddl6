59570730e92e5deb4c861bd62173d188f554fc6a
Krystian Pietroń <krystianpietr@gmail.com>
2018-11-30 12:06:38 +0100

    add checkIfMovesAreAvailable

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -6,7 +6,8 @@ class Snake extends React.Component {
         super(props)
 
         const halfBoardDimension = Math.ceil(props.boardDimension / 2) - 1
-
+        this.intervalId = null
+        this.currentGameBoard = null
         this.state = {
             gameBoard: (
                 Array(this.props.boardDimension)
@@ -32,10 +33,67 @@ class Snake extends React.Component {
             food: [
                 // { x: 2, y: 2 }
             ],
-            currentPlayerIndex: 0
+            currentPlayerIndex: 0,
+            gameTickTime: props.startGameTickTime
         }
 
     }
+    componentDidMount = () => (
+        this.intervalId = setInterval(
+            this.gameTick,
+            this.state.gameTickTime
+        )
+    )
+    componentWillMount = () => (
+        clearInterval(this.intervalId)
+    )
+    gameTick = () => (
+        console.log('Tick'),
+        this.checkIfMovesAreAvailable()
+    )
+    checkIfMovesAreAvailable = () => (
+        this.state.snakes.forEach(snakePositions, snakeIndex => {
+            const snakeHeadPosition = snakePositions[0]
+            const direction = this.state.directions[snakeIndex]
+            let newHeadPosition = null
+            switch (direction) {
+                case 'left':
+                    newHeadPosition = {
+                        x: snakeHeadPosition.x - 1,
+                        y: snakeHeadPosition.y,
+                    }
+                    break
+                case 'right':
+                    newHeadPosition = {
+                        x: snakeHeadPosition.x + 1,
+                        y: snakeHeadPosition.y,
+                    }
+                    break
+                case 'top':
+                    newHeadPosition = {
+                        x: snakeHeadPosition.x,
+                        y: snakeHeadPosition.y - 1,
+                    }
+                    break
+                case 'bottom':
+                    newHeadPosition = {
+                        x: snakeHeadPosition.x,
+                        y: snakeHeadPosition.y + 1,
+                    }
+                    break
+                    default:
+            }
+            if (
+                this.currentGameBoard[newHeadPosition.y] &&
+                this.currentGameBoard[newHeadPosition.y][newHeadPosition.x]
+            ){
+                this.moveSnake(snakeIndex)
+            }else{
+                this.endGame(snakeIndex)
+            }
+        }
+        )
+    )
     composeGameBoard = () => {
         const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
         this.state.snakes.forEach(snake => (
@@ -50,17 +108,18 @@ class Snake extends React.Component {
     }
 
     render() {
-        const gameBoard = this.composeGameBoard()
+        this.currentGameBoard = this.composeGameBoard()
 
         return (
-            <GameBoard gameBoard={gameBoard} />
+            <GameBoard gameBoard={this.currentGameBoard} />
         )
     }
 }
 
 Snake.defaultProps = {
     // @TODO is should be check if bigger than eg .5
-    boardDimension: 10
+    boardDimension: 10,
+    startGameTickTime: 1000
 }
 
 export default Snake
